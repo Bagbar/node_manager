ae08e3b7359cecfba0d138e6f1521db9c14d19b6
Bagbar <christian.konrad@rub.de>
2015-09-24 17:56:11 +0200

    script change

--- cluster_control/copy_complie.sh
+++ cluster_control/copy_complie.sh
@@ -5,11 +5,12 @@ mkdir "$HOME/$NEWDIR" -p
 rsync -r *.c "$HOME/$NEWDIR"
 rsync -r *.h "$HOME/$NEWDIR"
 cd "$HOME/$NEWDIR"
-gcc -std=c99 -O2 -c -o "basics.o" "basics.c"
-gcc -std=c99 -O2 -c -o "main.o" "main.c"
-gcc -std=c99 -O2 -c -o "master.o" "master.c"
-gcc -std=c99 -O2 -c -o "slave.o" "slave.c"
+gcc -std=c99 -I/usr/include/libxml2 -O2 -c -o "basics.o" "basics.c"
+gcc -std=c99 -I/usr/include/libxml2 -O2 -c -o "main.o" "main.c"
+gcc -std=c99 -I/usr/include/libxml2 -O2 -c -o "master.o" "master.c"
+gcc -std=c99 -I/usr/include/libxml2 -O2 -c -o "slave.o" "slave.c"
+gcc -std=c99 -I/usr/include/libxml2 -O2 -c -o "XML.o" "XML.c"
 
-gcc  -o "slave_background_process"  ./basics.o ./main.o ./master.o ./slave.o   -lpthread
+gcc  -o "cluster_control"  ./basics.o ./main.o ./master.o ./slave.o ./XML.o  -lpthread -lxml2
 
-cd ~/git/node_manager/slave_background_process
+cd ~/git/node_manager/cluster_control
